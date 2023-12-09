eb9d6ed3f7fa5c5ec879c675073f083a9c4c41d8
Allan-Borges <alanborges195@gmail.com>
2023-12-09 07:23:45 -0300

    Desafio Modelo de Dominio ORM

---
+++ src/main/java/com/alandev/desafio/SistemaDeEventoApplication.java
@@ -0,0 +1,13 @@
+package com.alandev.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alandev/desafio/SistemaDeEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alandev.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
